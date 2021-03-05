96bb61b72cfa98819f2654c5db3f4f626684502b
Haiyan Hazman <haiyantripleh@gmail.com>
2021-03-05 23:32:10 +0800

    Booking Form Design

--- app/src/main/java/com/example/group8/BookingForm.java
+++ app/src/main/java/com/example/group8/BookingForm.java
@@ -3,17 +3,126 @@ package com.example.group8;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.DatePicker;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.TimePicker;
+import android.widget.Toast;
 
-public class BookingForm extends AppCompatActivity {
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+public class BookingForm extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
+
+    TextView tvTime, tvDate;
+    int tHour, tMinute;
+    Calendar calendar;
+    DatePickerDialog.OnDateSetListener setListener;
+    String[] services;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_booking_form);
 
+        tvTime = findViewById(R.id.tv_timePicker);
+
+        tvTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Initialize time picker dialog
+                TimePickerDialog timePickerDialog = new TimePickerDialog(
+                        BookingForm.this,
+                        android.R.style.Theme_Holo_Light_Dialog_MinWidth,
+                        new TimePickerDialog.OnTimeSetListener() {
+                            @Override
+                            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                                //panggil hour and minute
+                                tHour = hourOfDay;
+                                tMinute = minute;
+                                //simpan jam dan minit dalam string
+                                String time = tHour + ":" + tMinute;
+                                //panggil 24hour format
+                                SimpleDateFormat f24hours = new SimpleDateFormat(
+                                        "HH:mm"
+                                );
+                                try {
+                                    Date date = f24hours.parse(time);
+                                    //panggil 24hours time format
+                                    SimpleDateFormat f12hours = new SimpleDateFormat(
+                                            "hh:mm aa"
+                                    );
+                                    //set pilihan time di tv
+                                    tvTime.setText(f12hours.format(date));
+                                } catch (ParseException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        },12,0,false
+                );
+                //display previous selected time
+                timePickerDialog.updateTime(tHour,tMinute);
+                //show dialog
+                timePickerDialog.show();
+            }
+        });
+
+        tvDate = findViewById(R.id.tv_datePicker);
+
+        Calendar calendar = Calendar.getInstance();
+        final int year = calendar.get(Calendar.YEAR);
+        final int month = calendar.get(Calendar.MONTH);
+        final int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+        tvDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DatePickerDialog datePickerDialog = new DatePickerDialog(
+                        BookingForm.this, android.R.style.Theme_Holo_Light_Dialog_MinWidth,setListener,
+                        year,month,day);
+                datePickerDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                datePickerDialog.show();
+            }
+        });
+
+        setListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                month = month+1;
+                String date = day+"/"+month+"/"+year;
+                tvDate.setText(date);
+            }
+        };
+
+        Spinner spinner = findViewById(R.id.sp_services);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.services, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
+
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        String text = parent.getItemAtPosition(position).toString();
+        Toast.makeText(parent.getContext(), text, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 
     @Override
@@ -25,4 +134,5 @@ public class BookingForm extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
     }
+
 }
