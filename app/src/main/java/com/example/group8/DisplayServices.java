a0b3f0bae8f380bc472e3ca0aee072f7dbff43a1
Amir syafiq <amirzamri01@gmail.com>
2021-03-09 23:25:31 +0800

    Our services design

---
+++ app/src/main/java/com/example/group8/DisplayServices.java
@@ -0,0 +1,14 @@
+package com.example.group8;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DisplayServices extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_services);
+    }
+}
